5d04b909578419cf892ac4b8c10facd1f2cf6eb1
Steven Frederiksen <steven@grafresearch.com>
2022-04-27 09:43:27 -0400

    Added some demo data

--- crypto_tool/rcli/src/main.rs
+++ crypto_tool/rcli/src/main.rs
@@ -1,18 +1,22 @@
 /// Encryption CLI
 /// Author: Steven Frederiksen
 /// Derived From: https://highassurance.rs/chp2/cli.html
-use chacha20poly1305::aead::{AeadInPlace, NewAead};
+use chacha20poly1305::aead::{Aead, AeadInPlace, NewAead, Error};
 use chacha20poly1305::{ChaCha20Poly1305, Key, Nonce};
 use clap::Parser;
 use std::fs::File;
 use std::io;
 use std::io::prelude::{Read, Seek, Write};
 
-/// RC4 file en/decryption
+/// File en/decryption
 #[derive(Parser, Debug)]
 struct Args {
+    // Tell the tool to only predict if the given file is encrypted
+    #[clap(short, long)]
+    predict_only: bool,
+
     /// Name of file
-    #[clap(short, long, required = true, value_name = "FILE_NAME")]
+    #[clap(short, long, value_name = "FILE_NAME", required=true)]
     file: String,
 
     /// En/Decryption key
@@ -20,7 +24,7 @@ struct Args {
         short,
         long,
         required = true,
-        value_name = "KEY",
+        value_name = "KEY"
     )]
     key: String,
 
@@ -29,51 +33,81 @@ struct Args {
         short,
         long,
         required = true,
-        value_name = "NONCE",
+        value_name = "NONCE"
     )]
     nonce: String,
 }
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    let mut contents = Vec::new();
-
-    // read in user arguments
-    // Note:
-    // Enfore length here
-    if args.key.len() != 32 {
-        return Err(io::Error::new(io::ErrorKind::Other, "Key must be 32 characters long"));
-    }
-    let key = Key::from_slice(args.key.as_bytes());
-    let nonce = Nonce::from_slice(args.nonce.as_bytes());
-
-    // create the cipher instance
-    let cipher = ChaCha20Poly1305::new(key);
 
     // Open the file for read/write
-    let mut file = File::options().read(true).write(true).open(&args.file)?;
+    let mut contents = Vec::new();
+    let mut file = File::options().read(true).open(&args.file)?;
 
     // Read the file
     file.read_to_end(&mut contents)?;
     let total_bits = contents.len() * 8;
     let mut total_ones = 0;
+    let mut total_below_128 = 0;
     for byte in &contents {
         total_ones += byte.count_ones();
+        if byte < &128 {
+            total_below_128 += 1;
+        }
     }
+    let ones_ratio = total_ones as f32 / total_bits as f32;
+    let ascii_ratio = total_below_128 as f32 / contents.len() as f32;
+    println!(
+        "Distribution of ones: {}/{} ({}%)",
+        total_ones,
+        total_bits,
+        ones_ratio * 100.0
+    );
+    println!(
+        "Distribution of < 128: {}/{} ({}%)",
+        total_below_128,
+        contents.len(),
+        ascii_ratio * 100.0
+    );
 
-    println!("Distribution of ones: {}/{} ({}%)", total_ones, total_bits, (total_ones as f32 / total_bits as f32) * 100.0);
+    let encrypt = ascii_ratio >= 0.990;
 
-    // En/Decrypt the file
-    cipher
-        .encrypt_in_place(nonce, b"", &mut contents)
-        .expect("encryption failure");
+    if !args.predict_only {
+        // read in user arguments
+        // Note:
+        // Enfore length here
+        if args.key.len() != 32 {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "Key must be 32 characters long",
+            ));
+        }
+        let key = Key::from_slice(args.key.as_bytes());
+        let nonce = Nonce::from_slice(args.nonce.as_bytes());
 
-    // Overwrite the existing file
-    file.rewind()?; // Start at the beginning
-    file.write_all(&contents)?;
+        // create the cipher instance
+        let cipher = ChaCha20Poly1305::new(key);
 
-    // Success
-    println!("Processed {}", args.file);
+        // En/Decrypt the file
+        let new_contents = if encrypt {
+            let ciphertext = cipher.encrypt(nonce, contents.as_slice());
+            println!("Encrypted {}", args.file);
+            ciphertext
+        } else {
+            let plaintext = cipher.decrypt(nonce, contents.as_slice());
+            println!("Decrypted {}", args.file);
+            plaintext
+        };
+
+        if let Err(e) = new_contents {
+            println!("Failure to process file {}", e);
+        } else {
+            // Overwrite the existing file check TODO
+            let mut file = File::options().write(true).open(format!("{}.enc", args.file))?;
+            file.write_all(&new_contents.unwrap())?;
+        }
+    }
 
     Ok(())
 }
